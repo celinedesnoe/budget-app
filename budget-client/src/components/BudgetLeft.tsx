f90501d7c2844280206a0774037261b258ecb597
Celine <celine.desnoe@weloop.io>
2020-08-26 17:35:24 +0200

    add font and color

--- budget-client/src/components/BudgetLeft.tsx
+++ budget-client/src/components/BudgetLeft.tsx
@@ -8,7 +8,12 @@ interface RootState {
 const BudgetLeft: FC = () => {
   const selectBudget = (state: RootState) => state.budget;
   const budget = useSelector(selectBudget);
-  return <div className="budget-left">{budget}</div>;
+  return (
+    <div className="budget-left">
+      <div>Solde restant</div>
+      <div className="bold">{budget}</div>
+    </div>
+  );
 };
 
 export default BudgetLeft;
